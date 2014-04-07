267dc866afb2cc1a40d7002db055c2ddeb347644
Thiago Carvalho <thiago.carvalho@westwing.com.br>
2014-04-07 16:05:19 -0300

    redesign landing

--- public/index.php
+++ public/index.php
@@ -1,22 +1,75 @@
+
 <!DOCTYPE html>
-<html>
-<head>
-    <title>Email</title>
-    <style type="text/css">
-        body {
-            font-family: sans-serif;
-        }
-    </style>
-</head>
-<body>
-    <h3>Email file to check. It must be a raw email file</h3>
-    <form action="upload.php" method="post" onsubmit="mixpanel.track('submit');" enctype="multipart/form-data">
-        <input type="file" name="email">
-        <input type="submit">
-    </form>
-    <hr>
-    Load time will be calculated from <strong>navigationStart</strong> to <strong>loadEventEnd.</strong>
-    <img src="timing-overview.png" width="1000">
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="shortcut icon" href="../../assets/ico/favicon.ico">
+
+    <title>Email Testing</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom styles for this template -->
+    <link href="css/jumbotron-narrow.css" rel="stylesheet">
+
+    <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+      <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
+  </head>
+
+  <body>
+
+    <div class="container">
+      <div class="header">
+        <!--<ul class="nav nav-pills pull-right">
+          <li class="active"><a href="#">Home</a></li>
+          <li><a href="#">About</a></li>
+          <li><a href="#">Contact</a></li>
+        </ul>-->
+        <h3 class="text-muted">Email Testing</h3>
+      </div>
+
+      <div class="jumbotron">
+        <!--<h1>Upload and Test:</h1>-->
+        <p class="lead">Add a raw email file and click Submit.</p>
+
+        <form action="upload.php" method="post" onsubmit="mixpanel.track('submit');" enctype="multipart/form-data">
+          <input type="file" name="email" style="display: inline-block;">
+          <input type="submit" class="btn btn-lg btn-success" value="Submit">
+        </form>
+      </div>
+
+      <div class="row marketing">
+        <div class="col-lg-12">
+          <h4>Getting the raw email</h4>
+          <img src="img/raw_email.png" style="box-shadow: 0px 0px 15px #cccccc;">
+          <p>On gmail click 'Show original' to get the raw email.</p>
+        </div>
+      <!--
+        <div class="col-lg-6">
+          <h4>Subheading</h4>
+          <p>Donec id elit non mi porta gravida at eget metus. Maecenas faucibus mollis interdum.</p>
+
+          <h4>Subheading</h4>
+          <p>Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Cras mattis consectetur purus sit amet fermentum.</p>
+
+          <h4>Subheading</h4>
+          <p>Maecenas sed diam eget risus varius blandit sit amet non magna.</p>
+        </div>
+      </div>-->
+
+      <div class="footer">
+        <p><a href="http://github.com/thiagooak" target="_blank">Thiago Carvalho</a> 2014</p>
+      </div>
+
+    </div> <!-- /container -->
 
     <?php if (php_sapi_name() != 'cli-server') { ?>
     <!-- start Mixpanel --><script type="text/javascript">(function(e,b){if(!b.__SV){var a,f,i,g;window.mixpanel=b;b._i=[];b.init=function(a,e,d){function f(b,h){var a=h.split(".");2==a.length&&(b=b[a[0]],h=a[1]);b[h]=function(){b.push([h].concat(Array.prototype.slice.call(arguments,0)))}}var c=b;"undefined"!==typeof d?c=b[d]=[]:d="mixpanel";c.people=c.people||[];c.toString=function(b){var a="mixpanel";"mixpanel"!==d&&(a+="."+d);b||(a+=" (stub)");return a};c.people.toString=function(){return c.toString(1)+".people (stub)"};i="disable track track_pageview track_links track_forms register register_once alias unregister identify name_tag set_config people.set people.set_once people.increment people.append people.track_charge people.clear_charges people.delete_user".split(" ");
@@ -27,5 +80,8 @@
         mixpanel.track("pageview");
     </script>
     <?php } ?>
-</body>
+    <!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+  </body>
 </html>
