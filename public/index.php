8ed5ffcc21a84efd4878e78c2e5e09f12d70bae0
Thiago Carvalho <thiago.carvalho@westwing.com.br>
2014-04-08 18:37:35 -0300

    added timing api info

--- public/index.php
+++ public/index.php
@@ -1,20 +1,13 @@
-
 <!DOCTYPE html>
 <html lang="en">
   <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="description" content="">
-    <meta name="author" content="">
-    <link rel="shortcut icon" href="../../assets/ico/favicon.ico">
 
     <title>Email Testing</title>
 
-    <!-- Bootstrap core CSS -->
     <link href="bootstrap/css/bootstrap.min.css" rel="stylesheet">
-
-    <!-- Custom styles for this template -->
     <link href="css/jumbotron-narrow.css" rel="stylesheet">
 
     <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
@@ -28,16 +21,10 @@
     <a href="https://github.com/thiagooak/email-testing" target="_blank"><img style="position: absolute; top: 0; right: 0; border: 0;" src="https://camo.githubusercontent.com/38ef81f8aca64bb9a64448d0d70f1308ef5341ab/68747470733a2f2f73332e616d617a6f6e6177732e636f6d2f6769746875622f726962626f6e732f666f726b6d655f72696768745f6461726b626c75655f3132313632312e706e67" alt="Fork me on GitHub" data-canonical-src="https://s3.amazonaws.com/github/ribbons/forkme_right_darkblue_121621.png"></a>
     <div class="container">
       <div class="header">
-        <!--<ul class="nav nav-pills pull-right">
-          <li class="active"><a href="#">Home</a></li>
-          <li><a href="#">About</a></li>
-          <li><a href="#">Contact</a></li>
-        </ul>-->
         <h3 class="text-muted">Email Testing</h3>
       </div>
 
       <div class="jumbotron">
-        <!--<h1>Upload and Test:</h1>-->
         <p class="lead">Add a raw email file and click Submit.</p>
 
         <form action="upload.php" method="post" onsubmit="mixpanel.track('submit');" enctype="multipart/form-data">
@@ -47,29 +34,25 @@
       </div>
 
       <div class="row marketing">
-        <div class="col-lg-12">
+        <div class="col-lg-6">
           <h4>Getting the raw email</h4>
-          <img src="img/raw_email.png" style="box-shadow: 0px 0px 15px #cccccc;">
+          <img src="img/raw_email.png" width="300" style="box-shadow: 0px 0px 15px #cccccc;">
           <p>On gmail click 'Show original' to get the raw email.</p>
         </div>
-      <!--
         <div class="col-lg-6">
-          <h4>Subheading</h4>
-          <p>Donec id elit non mi porta gravida at eget metus. Maecenas faucibus mollis interdum.</p>
-
-          <h4>Subheading</h4>
-          <p>Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Cras mattis consectetur purus sit amet fermentum.</p>
-
-          <h4>Subheading</h4>
-          <p>Maecenas sed diam eget risus varius blandit sit amet non magna.</p>
+          <h4>Measuring the load time</h4>
+          <img src="img/timing-overview.png" width="300" style="box-shadow: 0px 0px 15px #cccccc;">
+          <p>We use the <a href="http://www.w3.org/TR/navigation-timing/#sec-navigation-timing-interface" target="_blank">navigation timing api</a>
+            considering the time between <strong>navigationStart</strong> and <strong>loadEventEnd</strong>
+          </p>
         </div>
-      </div>-->
-
-      <div class="footer">
-        <p><a href="http://github.com/thiagooak" target="_blank">Thiago Carvalho</a> 2014</p>
       </div>
 
-    </div> <!-- /container -->
+      <div class="footer">
+        <p><a href="http://github.com/thiagooak" target="_blank">made with &#9829; in são paulo</a></p>
+      </div>
+
+    </div>
 
     <?php if (php_sapi_name() != 'cli-server') { ?>
     <!-- start Mixpanel --><script type="text/javascript">(function(e,b){if(!b.__SV){var a,f,i,g;window.mixpanel=b;b._i=[];b.init=function(a,e,d){function f(b,h){var a=h.split(".");2==a.length&&(b=b[a[0]],h=a[1]);b[h]=function(){b.push([h].concat(Array.prototype.slice.call(arguments,0)))}}var c=b;"undefined"!==typeof d?c=b[d]=[]:d="mixpanel";c.people=c.people||[];c.toString=function(b){var a="mixpanel";"mixpanel"!==d&&(a+="."+d);b||(a+=" (stub)");return a};c.people.toString=function(){return c.toString(1)+".people (stub)"};i="disable track track_pageview track_links track_forms register register_once alias unregister identify name_tag set_config people.set people.set_once people.increment people.append people.track_charge people.clear_charges people.delete_user".split(" ");
@@ -80,8 +63,5 @@
         mixpanel.track("pageview");
     </script>
     <?php } ?>
-    <!-- Bootstrap core JavaScript
-    ================================================== -->
-    <!-- Placed at the end of the document so the pages load faster -->
   </body>
 </html>
